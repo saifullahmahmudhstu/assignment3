41c7b753af4d0f0013b60719cae0befcc77730b5
Rimon <saifullahmahmud.hstu@gmail.com>
2022-03-10 14:56:27 +0600

    problem 2 add

---
+++ bin/FindPhone.dart
@@ -0,0 +1,47 @@
+
+class FindPhone {
+
+  void findIphone() {
+    List<Map> mobileData =
+    [
+
+      {
+        'brand_id': "113",
+        'brand_name': "TECNO",
+        'brand_image': "81761_Tecno-Mobile-logo-1.jpg",
+        'total_mobile': "1"
+      },
+      {
+        'brand_id': "112",
+        'brand_name': "Symphony",
+        'brand_image': "12537_shymponi.png",
+        'total_mobile': "2"
+      },
+      {
+        'brand_id': "111",
+        'brand_name': "Honor ",
+        'brand_image': "5729_honor-logo.png",
+        'total_mobile': "28"
+      },
+      {
+        'brand_id': "110",
+        'brand_name': "Realme",
+        'brand_image': "93081_relame.png",
+        'total_mobile': "17"
+      },
+      {
+        'brand_id': "109",
+        'brand_name': "iPhone",
+        'brand_image': "57914_zte.png",
+        'total_mobile': "10"
+      }
+    ];
+    var a;
+    for (var data in mobileData) {
+      if (mobileData[4]['brand_name'] == 'iPhone') {
+        a = mobileData[4]['brand_name'];
+      }
+    }
+    print(a);
+  }
+}
--- bin/assignment_03.dart
+++ bin/assignment_03.dart
@@ -1,13 +1,19 @@
-    import 'AbstractClass.dart';
+import 'AbstractClass.dart';
 import 'Computer.dart';
+import 'FindPhone.dart';
 import 'Student.dart';
 import 'super_keyword.dart';
 
 void main() {
     //number-1
+  var phone = FindPhone();
+  phone.findIphone();
+
+
+  //Problem No: 02
     //findAzadVai();
-    //Problem No: 02
-    findIphone();
+
+
    //Problem No: 03
       var stdObj1 =Student('John', 2);
       print(stdObj1.name);
@@ -49,50 +55,6 @@ void main() {
 
     }
 
-    void findIphone() {
-
-      List<Map> mobileData =
-      [
-
-        {
-          'brand_id': "113",
-          'brand_name': "TECNO",
-          'brand_image': "81761_Tecno-Mobile-logo-1.jpg",
-          'total_mobile': "1"
-        },
-        {
-          'brand_id': "112",
-          'brand_name': "Symphony",
-          'brand_image': "12537_shymponi.png",
-          'total_mobile': "2"
-        },
-        {
-          'brand_id': "111",
-          'brand_name': "Honor ",
-          'brand_image': "5729_honor-logo.png",
-          'total_mobile': "28"
-        },
-        {
-          'brand_id': "110",
-          'brand_name': "Realme",
-          'brand_image': "93081_relame.png",
-          'total_mobile': "17"
-        },
-        {
-          'brand_id': "109",
-          'brand_name': "iPhone",
-          'brand_image': "57914_zte.png",
-          'total_mobile':"10"
-        }
-      ];
-
-     for(var data in mobileData){
-       if(mobileData[4]['brand_name']=='iPhone'){
-         print(mobileData[4]['brand_name']);
-         break;
-       }
-     }
-    }
 
 
 
